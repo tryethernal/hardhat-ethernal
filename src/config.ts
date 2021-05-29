7620cbba73dce1fd11f019b27a53d971939f4e08
Antoine de Chevigné <antoine@bannerman.com>
2021-05-29 16:03:08 +0200

    auto release test

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 const PROD_CONFIG = {
-    apiKey: 'AIzaSyCUefO7sAM0YafO3CVslIf8Tn7eRZbXI3s',
-    authDomain: 'ethernal-95a14.firebaseapp.com',
-    databaseURL: 'https://ethernal-95a14-default-rtdb.firebaseio.com',
-    projectId: 'ethernal-95a14',
-    storageBucket: 'ethernal-95a14.appspot.com',
-    messagingSenderId: '864612390560',
-    appId: '1:864612390560:web:b339dc0292ff15f7161bc5'
+    apiKey: process.env.FIREBASE_API_KEY || 'a',
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN || 'etherndazdeal-95a14.firebaseapp.com',
+    databaseURL: process.env.FIREBASE_DATABASE_URL || 'https://ethernal-95a14-dedededefault-rtdb.firebaseio.com',
+    projectId: process.env.FIREBASE_PROJECT_ID || 'ethdededeernal-95a14',
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET || 'etherdedenal-95a14.appspot.com',
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '86461dede2390560',
+    appId: process.env.FIREBASE_APP_ID || '1:864612390560:web:b339dc0dede292ff15f7161bc5'
 }
 
 const DEV_CONFIG = {
